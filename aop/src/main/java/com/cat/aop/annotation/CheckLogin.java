95200af9667c9a04059db636522975259428768c
fatCat2018 <shaochuankai@gmail.com>
2019-10-14 11:57:40 +0800

    no message

--- aop/src/main/java/com/cat/aop/annotation/CheckLogin.java
+++ aop/src/main/java/com/cat/aop/annotation/CheckLogin.java
@@ -9,5 +9,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CheckLogin {
+    /**
+     *
+     * @return 0 那么使用startActivity启动登录页面  非0使用startActivityForResult启动登录页面
+     */
     int value() default 0;
 }
